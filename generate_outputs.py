d77f57503c04e89d0cf122bdf2a16965c17a4fb4
Isaack Mwenda <mwendaisaac@gmail.com>
2020-04-01 12:55:53 +0300

    rm g-cloud credentials arg

--- generate_outputs.py
+++ generate_outputs.py
@@ -15,9 +15,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Runs the post-fetch phase of the pipeline")
 
     parser.add_argument("user", help="User launching this program")
-    parser.add_argument("google_cloud_credentials_file_path", metavar="google-cloud-credentials-file-path",
-                        help="Path to a Google Cloud service account credentials file to use to access the "
-                             "credentials bucket")
     parser.add_argument("pipeline_configuration_file_path", metavar="pipeline-configuration-file",
                         help="Path to the pipeline configuration json file")
 
@@ -48,7 +45,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     user = args.user
-    google_cloud_credentials_file_path = args.google_cloud_credentials_file_path
     pipeline_configuration_file_path = args.pipeline_configuration_file_path
 
     raw_data_dir = args.raw_data_dir
