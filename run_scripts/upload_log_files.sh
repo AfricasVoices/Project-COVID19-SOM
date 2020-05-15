4fb871e0144e1cdcdc215272a1292d461c87fa5a
Isaack Mwenda <mwendaisaac@gmail.com>
2020-05-15 19:28:40 +0300

    upload logs every 24 hours

---
+++ run_scripts/upload_log_files.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+USER=$1
+GOOGLE_CLOUD_CREDENTIALS_FILE_PATH=$2
+PIPELINE_CONFIGURATION_FILE_PATH=$3
+MEMORY_PROFILE_DIR=$4
+DATA_ARCHIVE_DIR=$5
+
+cd ..
+while true; do
+    pipenv run python upload_log_files.py "$USER" "$GOOGLE_CLOUD_CREDENTIALS_FILE_PATH" "$PIPELINE_CONFIGURATION_FILE_PATH" \
+           "$MEMORY_PROFILE_DIR" "$DATA_ARCHIVE_DIR"
+    echo "sleeping for 24 hours"
+    sleep 24h
+done
